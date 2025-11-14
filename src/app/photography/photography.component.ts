43b59a7f9dfa5ce005a5f5d35e6a8a4a4b9e0103
milkywayhalo <codingexpert@mail.de>
2025-11-14 20:16:31 +0100

    Add image to navbar, worked on gallery

--- src/app/photography/photography.component.ts
+++ src/app/photography/photography.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { GalleriaModule } from 'primeng/galleria';
 
 @Component({
   selector: 'app-photography',
   standalone: true,
-  imports: [],
+  imports: [GalleriaModule],
   templateUrl: './photography.component.html',
-  styleUrl: './photography.component.scss'
+  styleUrl: './photography.component.scss',
 })
 export class PhotographyComponent {
+  images = model([]);
 
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '1300px',
+      numVisible: 4,
+    },
+    {
+      breakpoint: '575px',
+      numVisible: 1,
+    },
+  ];
 }
